59d316500f7ef2628c184092778b096a66959a43
Sourabh Adap <adapsourbh@gmail.com>
2024-06-01 19:11:36 +0530

    done with task 3

--- pyls/__main__.py
+++ pyls/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import time
 from typing import List, Dict, Any
 
 
@@ -9,19 +10,32 @@ def load_json_file(file_path: str) -> Dict[str, Any]:
         return json.load(file)
 
 
-def list_directory_contents(directory: Dict[str, Any], show_all: bool = False) -> List[str]:
+def list_directory_contents(directory: Dict[str, Any], show_all: bool = False) -> List[Dict[str, Any]]:
     items = []
     for item in directory.get('contents', []):
         if not show_all and item['name'].startswith('.'):
             continue
-        items.append(item['name'])
+        items.append(item)
     return items
+
+
+def format_time(epoch_time: int) -> str:
+    return time.strftime('%b %d %H:%M', time.localtime(epoch_time))
+
+
+def print_long_format(contents: List[Dict[str, Any]]) -> None:
+    for item in contents:
+        permissions = item['permissions']
+        size = item['size']
+        time_modified = format_time(item['time_modified'])
+        name = item['name']
+        print(f"{permissions} {size:>5} {time_modified} {name}")
 
 
 def main():
     parser = argparse.ArgumentParser(description="Python ls utility.")
-    parser.add_argument('-A', action='store_true', help='Include directory entries whose names begin with a dot (.)',
-                        default=False)
+    parser.add_argument('-A', action='store_true', help='Include directory entries whose names begin with a dot (.)')
+    parser.add_argument('-l', action='store_true', help='Use a long listing format')
     parser.add_argument('path', nargs='?', default='structure.json',
                         help='Path to the JSON file representing the directory structure')
 
@@ -33,7 +47,12 @@ def main():
 
     directory_structure = load_json_file(args.path)
     contents = list_directory_contents(directory_structure, show_all=args.A)
-    print(' '.join(contents))
+
+    if args.l:
+        print_long_format(contents)
+    else:
+        names = [item['name'] for item in contents]
+        print(' '.join(names))
 
 
 if __name__ == '__main__':
--- tests/test_pyls.py
+++ tests/test_pyls.py
@@ -1,5 +1,7 @@
 import unittest
-from pyls.__main__ import list_directory_contents
+from io import StringIO
+import sys
+from pyls.__main__ import list_directory_contents, print_long_format
 
 
 class TestPyls(unittest.TestCase):
@@ -24,14 +26,53 @@ class TestPyls(unittest.TestCase):
         }
 
     def test_list_directory_contents_without_dotfiles(self):
-        expected = ['LICENSE', 'README.md', 'ast', 'go.mod', 'lexer', 'main.go', 'parser', 'token']
+        expected = [
+            {'name': 'LICENSE', 'size': 1071, 'time_modified': 1699941437, 'permissions': 'drwxr-xr-x'},
+            {'name': 'README.md', 'size': 83, 'time_modified': 1699941437, 'permissions': 'drwxr-xr-x'},
+            {'name': 'ast', 'size': 4096, 'time_modified': 1699957739, 'permissions': '-rw-r--r--'},
+            {'name': 'go.mod', 'size': 60, 'time_modified': 1699950073, 'permissions': 'drwxr-xr-x'},
+            {'name': 'lexer', 'size': 4096, 'time_modified': 1699955487, 'permissions': 'drwxr-xr-x'},
+            {'name': 'main.go', 'size': 74, 'time_modified': 1699950453, 'permissions': '-rw-r--r--'},
+            {'name': 'parser', 'size': 4096, 'time_modified': 1700205662, 'permissions': 'drwxr-xr-x'},
+            {'name': 'token', 'size': 4096, 'time_modified': 1699954070, 'permissions': '-rw-r--r--'}
+        ]
         result = list_directory_contents(self.structure)
         self.assertEqual(result, expected)
 
     def test_list_directory_contents_with_dotfiles(self):
-        expected = ['.gitignore', 'LICENSE', 'README.md', 'ast', 'go.mod', 'lexer', 'main.go', 'parser', 'token']
+        expected = [
+            {'name': '.gitignore', 'size': 8911, 'time_modified': 1699941437, 'permissions': 'drwxr-xr-x'},
+            {'name': 'LICENSE', 'size': 1071, 'time_modified': 1699941437, 'permissions': 'drwxr-xr-x'},
+            {'name': 'README.md', 'size': 83, 'time_modified': 1699941437, 'permissions': 'drwxr-xr-x'},
+            {'name': 'ast', 'size': 4096, 'time_modified': 1699957739, 'permissions': '-rw-r--r--'},
+            {'name': 'go.mod', 'size': 60, 'time_modified': 1699950073, 'permissions': 'drwxr-xr-x'},
+            {'name': 'lexer', 'size': 4096, 'time_modified': 1699955487, 'permissions': 'drwxr-xr-x'},
+            {'name': 'main.go', 'size': 74, 'time_modified': 1699950453, 'permissions': '-rw-r--r--'},
+            {'name': 'parser', 'size': 4096, 'time_modified': 1700205662, 'permissions': 'drwxr-xr-x'},
+            {'name': 'token', 'size': 4096, 'time_modified': 1699954070, 'permissions': '-rw-r--r--'}
+        ]
         result = list_directory_contents(self.structure, show_all=True)
         self.assertEqual(result, expected)
+
+    def test_print_long_format(self):
+        contents = list_directory_contents(self.structure)
+        expected_output = (
+            "drwxr-xr-x  1071 Nov 14 11:27 LICENSE\n"
+            "drwxr-xr-x    83 Nov 14 11:27 README.md\n"
+            "-rw-r--r--  4096 Nov 14 15:58 ast\n"
+            "drwxr-xr-x    60 Nov 14 13:51 go.mod\n"
+            "drwxr-xr-x  4096 Nov 14 15:21 lexer\n"
+            "-rw-r--r--    74 Nov 14 13:57 main.go\n"
+            "drwxr-xr-x  4096 Nov 17 12:51 parser\n"
+            "-rw-r--r--  4096 Nov 14 14:57 token\n"
+        )
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        print_long_format(contents)
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(captured_output.getvalue(), expected_output)
 
 
 if __name__ == '__main__':
