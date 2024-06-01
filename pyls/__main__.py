73e28f392aca8d768d7f39701c82384ee7c22898
Sourabh Adap <adapsourbh@gmail.com>
2024-06-01 20:05:21 +0530

    done with task 5

--- pyls/__main__.py
+++ pyls/__main__.py
@@ -10,20 +10,27 @@ def load_json_file(file_path: str) -> Dict[str, Any]:
         return json.load(file)
 
 
-def list_directory_contents(directory: Dict[str, Any], show_all: bool = False, reverse: bool = False) -> List[
-    Dict[str, Any]]:
+def list_directory_contents(directory: Dict[str, Any], show_all: bool = False, reverse: bool = False,
+                            sort_time: bool = False) -> List[Dict[str, Any]]:
     items = []
     for item in directory.get('contents', []):
         if not show_all and item['name'].startswith('.'):
             continue
+        if sort_time:
+            item['time_modified'] = time.localtime(item['time_modified'])
         items.append(item)
+    if sort_time:
+        items = sorted(items, key=lambda t: time.mktime(t['time_modified']))
     if reverse:
-        items.sort(key=lambda x: x['name'], reverse=True)
+        items = items[::-1]
     return items
 
 
-def format_time(epoch_time: int) -> str:
-    return time.strftime('%b %d %H:%M', time.localtime(epoch_time))
+def format_time(epoch_time: Any) -> str:
+    if isinstance(epoch_time, int):
+        return time.strftime('%b %d %H:%M', time.localtime(epoch_time))
+    else:
+        return time.strftime('%b %d %H:%M', epoch_time)
 
 
 def print_long_format(contents: List[Dict[str, Any]]) -> None:
@@ -39,7 +46,8 @@ def main():
     parser = argparse.ArgumentParser(description="Python ls utility.")
     parser.add_argument('-A', action='store_true', help='Include directory entries whose names begin with a dot (.)')
     parser.add_argument('-l', action='store_true', help='Use a long listing format')
-    parser.add_argument('-r', action='store_true', help='List subdirectories recursively', default=False)
+    parser.add_argument('-r', action='store_true', help='List subdirectories reverse', default=False)
+    parser.add_argument('-t', action='store_true', help='List subdirectories by modified time', default=False)
     parser.add_argument('path', nargs='?', default='structure.json',
                         help='Path to the JSON file representing the directory structure')
 
@@ -50,7 +58,7 @@ def main():
         return
 
     directory_structure = load_json_file(args.path)
-    contents = list_directory_contents(directory_structure, show_all=args.A, reverse=args.r)
+    contents = list_directory_contents(directory_structure, show_all=args.A, reverse=args.r, sort_time=args.t)
 
     if args.l:
         print_long_format(contents)
--- tests/test_pyls.py
+++ tests/test_pyls.py
@@ -94,6 +94,26 @@ class TestPyls(unittest.TestCase):
 
         self.assertEqual(captured_output.getvalue(), expected_output)
 
+    def test_print_long_format_in_reverse_with_time_modified(self):
+        contents = list_directory_contents(self.structure, reverse=True, sort_time=True)
+        expected_output = (
+            "drwxr-xr-x  4096 Nov 17 12:51 parser\n"
+            "-rw-r--r--  4096 Nov 14 15:58 ast\n"
+            "drwxr-xr-x  4096 Nov 14 15:21 lexer\n"
+            "-rw-r--r--  4096 Nov 14 14:57 token\n"
+            "-rw-r--r--    74 Nov 14 13:57 main.go\n"
+            "drwxr-xr-x    60 Nov 14 13:51 go.mod\n"
+            "drwxr-xr-x    83 Nov 14 11:27 README.md\n"
+            "drwxr-xr-x  1071 Nov 14 11:27 LICENSE\n"
+        )
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        print_long_format(contents)
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(captured_output.getvalue(), expected_output)
+
 
 if __name__ == '__main__':
     unittest.main()
